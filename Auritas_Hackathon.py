7d7966f6d9fe1ffa7ecc2dd5e0e6757c1f927d50
PapiFab1 <144495327+PapiFab1@users.noreply.github.com>
2025-10-25 03:57:43 -0400

    basic setup for what we need going in for tomorrow

--- Auritas_Hackathon.py
+++ Auritas_Hackathon.py
@@ -1,3 +1,13 @@
-#backend
-def thisIsTest():
-    return
+import pandas as pd
+from tkinter import *
+
+#needs: need a way for gui to access all csv's and figure out which csvs correspond to which data according to sap.
+#Each CSV file given is information regarding a certain button or category on the sample GUI png.
+
+#instantiate an instance of a window; main loop displays it on screen
+window = Tk()
+window.mainloop()
+
+#simple csv reader, will display first five layers of csv
+dataFrame = pd.read_csv("CSV Files/CDHDR.csv")
+print(dataFrame.head())
